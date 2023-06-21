26ec8b683e4ffc30366b0099f538a0dbd44d161e
unknown <andrew.ndrw1@gmail.com>
2023-06-21 10:45:18 +0300

    chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,44 +1,70 @@
 import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import { Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  const {repos} = React.useContext(GithubContext)
-  let Languages = repos.reduce((total, item) => {
-    const {language} = item
-    if (!language) return total
-    if (!total[language]) {
-      total[language] = {label: language, value: 1}
-    } else {
-      total[language] = {...total[language], value: total[language].value + 1}
-    }
-    return total
-  }, {
+  const { repos } = React.useContext(GithubContext);
 
-  })
-  Languages = Object.values(Languages).sort((a, b) => {
-    return b.value - a.value
-  }).slice(0, 5)
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13"
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
+    }
+    return total;
+  }, {});
+
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  // most stars per language
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  // stars, forks
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
     },
     {
-      label: "CSS",
-      value: "160"
-    },
-    {
-      label: "Javascript",
-      value: "80"
-    },
-  ];
-  return <section className='section'>
-    <Wrapper className='section-center'>
-      <Pie3D data={Languages}></Pie3D>
-    {/* <ExampleChart data={chartData}></ExampleChart> */}
-    </Wrapper>
-  </section>
+      stars: {},
+      forks: {},
+    }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -12,9 +12,25 @@ const GithubProvider = ({children}) => {
     const [githubUser, setGithubUser] = useState(mockUser)
     const [repos, setRepos] = useState(mockRepos)
     const [followers, setFollowers] = useState(mockFollowers)
+    const [requests, setRequests] = useState(0)
+    const [loading, setIsLoading] = useState(false)
+    const [error, setError] = useState({show: false, msg: ""})
+    const checkRequests = () => {
+        axios(`${rootUrl}/rate_limit`).then(({data}) => {
+            let {rate: {remaining}} = data
+            setRequests(remaining)
+            if (remaining === 0) {
+                toggleError(true, 'sorry, you have exceeded your hourly rate limit!')
+            }
+        }).catch((err) => console.log(err))
+    }
+    function toggleError(show = false, msg = '') {
+        setError({show, msg})
+    }
+    useEffect(checkRequests, [])
     return (
         <GithubContext.Provider value={{
-            githubUser, repos, followers
+            githubUser, repos, followers, requests, error
         }}>{children}</GithubContext.Provider>
     )
 }
